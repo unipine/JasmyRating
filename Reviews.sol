94828133c9a5e8447f62517162157b947e7078e9
unipine <unipine29@gmail.com>
2021-03-11 15:40:43 +0200

    Update Reviews.sol

--- Reviews.sol
+++ Reviews.sol
@@ -16,23 +16,14 @@ contract Reviews {
     mapping(address => Profile) public profiles; // stores all profiles. key=profileID , value=profile struct
     uint256 public profilesCount; // amount of profiles in the mapping
 
-    // function addProfile(string memory _desc) public { // sender adds new profile
-    //     profilesCount++;
-    //     Profile storage p = profiles[_adrs];
-    //     p.id = profilesCount;
-    //     p.adrs = msg.sender;
-    //     p.description = _desc;
-    //     p.reviewsCount = 0;
-    // }
-
-    function addProfile(address _adrs, string memory _desc) public {
-        profilesCount++;
-        Profile storage p = profiles[_adrs];
-        p.id = profilesCount;
-        p.adrs = _adrs; // msg.sender;
-        p.description = _desc;
-        p.reviewsCount = 0;
-    }
+    function addProfile(string memory _desc) public { // sender adds new profile
+         profilesCount++;
+         Profile storage p = profiles[_adrs];
+         p.id = profilesCount;
+         p.adrs = msg.sender;
+         p.description = _desc;
+         p.reviewsCount = 0;
+     }
 
     function editProfile(address _inputAdrs, string memory _desc) public {
         require(_inputAdrs == msg.sender);
@@ -40,29 +31,11 @@ contract Reviews {
         profiles[_inputAdrs].description = _desc;
     }
 
-    // function addReview(address _profileAdrs,uint8 _review) public { // sender adds new review
-    //     require(_review > 0 && _review < 6); // valid review from 1 to 5
-    //     require(profiles[_profileAdrs].reviews[msg.sender] == 0); // sender can rate someone else only once
-
-    //     profiles[_profileAdrs].reviews[msg.sender] = _review;
-    //     profiles[_profileAdrs].reviewsCount++;
-    //     profiles[_profileAdrs].reviewsSum += _review;
-    //     profiles[_profileAdrs].average = uint8(
-    //         profiles[_profileAdrs].reviewsSum /
-    //             profiles[_profileAdrs].reviewsCount
-    //     );
-    // }
-
-    function addReview(
-        address _fromAdrs,
-        address _profileAdrs,
-        uint8 _review
-    ) public {
-        // _fromAdrs == msg.sender
+    function addReview(address _profileAdrs,uint8 _review) public { // sender adds new review
         require(_review > 0 && _review < 6); // valid review from 1 to 5
-        require(profiles[_profileAdrs].reviews[_fromAdrs] == 0); // sender can rate someone else only once
+        require(profiles[_profileAdrs].reviews[msg.sender] == 0); // sender can rate someone else only once
 
-        profiles[_profileAdrs].reviews[_fromAdrs] = _review;
+        profiles[_profileAdrs].reviews[msg.sender] = _review;
         profiles[_profileAdrs].reviewsCount++;
         profiles[_profileAdrs].reviewsSum += _review;
         profiles[_profileAdrs].average = uint8(
@@ -70,8 +43,6 @@ contract Reviews {
                 profiles[_profileAdrs].reviewsCount
         );
     }
-
     constructor() public {
-        // addProfile(msg.sender, "test");
     }
 }
